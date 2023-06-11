a631ef4d03269ffdc85672bce4b7454a50f1a756
Alexis Vallecillo <thsisiaavv@proton.me>
2023-06-10 18:17:27 -0600

    refactor: changing output

--- programming-language-1/calculadora-sueldo-obrero.cpp
+++ programming-language-1/calculadora-sueldo-obrero.cpp
@@ -5,28 +5,36 @@ float multa_obrero(int);
 float seguro_social(int);
 
 int main(int argc, char *argv[]) {
-	int piezas_fabricadas, piezas_faltantes;
-	float multa, sueldo_bruto, sueldo_neto, descuento_seguro_social;
+	int piezas_minimas, piezas_fabricadas, piezas_faltantes;
+	float multa, sueldo_bruto, sueldo_neto, deduccion_seguro_social;
+
+	piezas_minimas = 200;
 	piezas_faltantes = 0;
 	multa = 0;
 
-	std::cout << "Cuantas piezas frabrico el obrero: ";
+	std::cout << "[ENTRADA] ¿Cuántas piezas fabricó?: \n> ";
 	std::cin >> piezas_fabricadas;
+	std::cout << "------------------------------------------------------------";
+
 
 	
-	if (piezas_fabricadas >= 200)
+	if (piezas_fabricadas >= piezas_minimas)
 		sueldo_bruto = piezas_fabricadas * 70;
 	else {
 		piezas_faltantes = 200 - piezas_fabricadas;
 		multa = multa_obrero(piezas_faltantes);
 		sueldo_bruto = (piezas_fabricadas * 70) - multa;
-		descuento_seguro_social = seguro_social(sueldo_bruto);
-		sueldo_neto = 0;
+		deduccion_seguro_social = seguro_social(sueldo_bruto);
+		sueldo_neto = sueldo_bruto - deduccion_seguro_social;
 	}
 
-	std::cout << "Sueldo Bruto: " << sueldo_bruto << " el faltante de piezas es " << piezas_faltantes << " y la multa es de " << multa << ".\n";
-	std::cout << "Seguro Social: " << seguro_social << "\n";
-	std::cout << "Sueldo Neto: " << sueldo_neto << "\n";
+	std::cout << "\n\nCantidad miníma de piezas: " << piezas_minimas;
+	std::cout << "\nPiezas fabricadas: " << piezas_fabricadas;
+	std::cout << "\nPiezas faltantes: " << piezas_faltantes;
+	std::cout << "\nMulta: " << multa;
+	std::cout << "\n\nSueldo bruto: " << sueldo_bruto; 
+	std::cout << "\nSeguro social: " << seguro_social;
+	std::cout << "\nSueldo neto: " << sueldo_neto << "\n";
 
 	return 0;
 }
